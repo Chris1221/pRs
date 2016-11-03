85242730749802d7072403c86c63356d94e8c2aa
Chris C <chris.c.1221@gmail.com>
2016-11-03 12:58:11 +0000

    update aprs, see changelog

--- inst/analysis/hdl.R
+++ inst/analysis/hdl.R
@@ -7,4 +7,11 @@ hdl <- fread("/scratch/hpc2862/lshtm/aprs/summary_data/jointGwasMc_LDL.txt", h =
 
 hdl <- hdl[hdl$P < 0.05,]
 
+cad <- fread("cad.add.fixrand.dgc.anno.160614.out.txt", h = T)
+
+cad %<>% select(legendrs, beta) %>% mutate(rsid = legendrs) %>% select(-legendrs)
+hdl %<>% select(rsid, A2)
+
+hdl <- merge(hdl, cad, by = "rsid")
+
 hdl %>% select(rsid, A2, beta) %>% mutate(A2 = toupper(A2)) %>% write.table(file = "~/repos/aprs/data-raw/hdl_0.05.raw", col.names = F, row.names = F, quote = F)
--- inst/analysis/tg.R
+++ inst/analysis/tg.R
@@ -7,4 +7,11 @@ tg <- fread("/scratch/hpc2862/lshtm/aprs/summary_data/jointGwasMc_LDL.txt", h = 
 
 tg <- tg[tg$P < 0.05,]
 
+cad <- fread("cad.add.fixrand.dgc.anno.160614.out.txt", h = T)
+
+cad %<>% select(legendrs, beta) %>% mutate(rsid = legendrs) %>% select(-legendrs)
+tg %<>% select(rsid, A2)
+
+tg <- merge(tg, cad, by = "rsid")
+
 tg %>% select(rsid, A2, beta) %>% mutate(A2 = toupper(A2)) %>% write.table(file = "~/repos/aprs/data-raw/tg_0.05.raw", col.names = F, row.names = F, quote = F)
