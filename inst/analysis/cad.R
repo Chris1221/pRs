6e692bf735f8f0374d2edda0f7d1964645e8a910
Chris C <chris.c.1221@gmail.com>
2016-11-03 13:16:20 +0000

    update aprs, see changelog

---
+++ inst/analysis/cad.R
@@ -0,0 +1,7 @@
+library(data.table)
+library(dplyr)
+library(magrittr)
+
+cad <- fread("~/cad.add.fixrand.dgc.anno.160614.out.txt", h = T)
+
+cad %>% filter(q_pvalue < 0.05) %>% filter(legendrs, other_allele, beta) %>% write.table(file = "~/repos/aprs/data-raw/cad_q0.05.raw", h = F)
