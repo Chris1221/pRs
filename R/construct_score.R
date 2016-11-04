14022284044fcc628285637a1bce7ca7f8ef1368
Chris C <chris.c.1221@gmail.com>
2016-11-04 13:43:34 +0000

    update aprs, see changelog

--- R/construct_score.R
+++ R/construct_score.R
@@ -1,63 +1,66 @@
-#' @title Construct a PRS
+#' @title Construct a Simple Polygenic Risk Score
 #' 
-#' @description Given input genetic files and association statistics, generate one of a number of polygenic risk scores. See \code{mode}. Tries to guess between plink input and Oxford format.
+#' @description Given input genetic files and association statistics, generate one of a number of polygenic risk scores. 
 #'
-#' @details This is helper code to construct a simple PRS in R. Note that it is currently unoptimized, meaning that it is currently implimented in R and not RcppArmadillo as it will be in the future. 
-#' First it reads in.
+#' @details This is helper code to construct a simple PRS in R. Note that it is currently unoptimized, meaning that it is currently implimented in R and not RcppArmadillo as it will be in the future. \cr 
+#' 
+#' In order to use this function, you must convert your genetic files to be \code{plink.raw} format. This serves several purposes:
+#' \itemize{
+#' 	\item It allows this function to have a standard set of inputs which are predictable and easy to deal with. 
+#'	\item It is an efficient format which is easy to deal with computationally.
+#' 	\item Avoids any shell integration, which makes the functions more cross-platform.
+#' } \cr
+#' We understand that this represents an additional processing step, but we think that the reward is worth the time. If you've done this kind of thing before, please use the \code{--recodeA} flag to produce a single \code{plink.raw} file. If you have not, we have provided a vignette on data conversion and the same information in the package wiki online. \cr
+#' If \code{mode == "single"}, then the function assumes that you wish to construct the scores using weighting coefficients from the *same disorder* (i.e. using the weights provided in the \code{assoc} file) and those P values for thresholding. If \code{mode == "multiple"} then the function assumes that you wish to use the original P values in \code{assoc} to perform SNP selection, but an alternate set of weights. Provide this alternate set of weights through \code{alternate_weights}.  \cr
 #'
-#' @param file Basename for genetic files (i.e. ${file}.ped ${file}.map). These must be unzipped. If non-standard, please specify seperately.
-#' @param mode One of c("weighted", "unweighted", "additive"). For more details see \code{details}.
-#'
+#' The inclusion criteria for the score are provided with the \code{p} arguement. All variants with P < p will be included in the score.
+#' @param file Path to converted raw plink file. This is a typical \code{plink} file which has been converted to an R readable format through \code{recodeA}. See \code{details} for specific conversion instructions. 
+#' @param mode One of "single" or "multiple". For more details see \code{details}.
+#' @param assoc File path to summary statistics used for score construction. This file must contain a column named "P" or "pvalue" or "p-value" or something similar. If the beta coefficients are to be used from this file (as opposed to \code{alternate_weights}) then a column named "beta", "Beta", "B", or similar must be present. See \code{details} for more information.
+#' @param alternate_weights File path to an alternate weighting scheme for use in cross-disorder PRS. 
+#' @param p P value cutoff for SNP inclusion in PRS.
+#' 
+#' @importFrom data.table fread
 #' @importFrom assertthat assert_that
 #' @import futile.logger
 #'
 #' @useDynLib aprs
 #'
-#' @return An S4 score object.
+#' @return An S4 PRS object. Slots include: \cr
+#' \itemize{
+#'	\item "score" - A data.frame with column 1 being FID, column 2 being IID, and column 3 being SCORE. Similar to \code{plink} output. 
+#' }
 
 construct_score <- function(
 			    file = NULL,
-			    ped = NULL,
-			    map = NULL,
 			    assoc = NULL,
-			    mode
+			    alternate_weights = NULL,
+			    mode = "single",
+			    p = 0.05
 			    ){
 	
 	flog.threshold(INFO)
 	flog.info("Checking inputs for inconsistencies")
-
-	# Make sure that either
-	#	file is defined (i.e. a file base is provided)
-	# or 
-	#	both ped and map are provided
-	# in order to have all information.
-	assert_that(!is.null(file) || ( !is.null(ped) && !is.null(map) ) )
-
-	# Make sure that some kind of weight is provided if 
-	# 	mode == "weighted" || "comorbid"
-	# Not neccessary if 
-	#	mode == "unweighted"
-	assert_that(!is.null(assoc) || mode == "unweighted")
-
-
-	flog.info("Reading in and processing.")
-	flog.trace("Using R to read for now, can't figure out the cpp")
 	
-	# Read in using R
-	if(!is.null(file)) { # if base is specified
+	# Make sure that the file is present
+	assert_that(!is.null(file)) 
+	# Make sure that some kind of weight is provided if 
+	# 	mode == "single"
+	# Make sure that the alternate weights are provided if 
+	#	mode == "multiple"
+	
+	if(mode == "single"){
 
-	# Specify map and ped paths through the base
-		map <- paste0(file, ".map")
-		ped <- paste0(file, ".ped")
+		assert_that(!is.null(assoc))
+		
+		flog.info("Reading in and formatting the raw file.")
+	
+		raw <- fread(file, h = T)
 
-	} # END map/ped.
+		
 
-	# if the file was null then map and ped are already specified.
-
-	map <- fread(map, h = T)
-	ped <- fread(ped, h = T)
-
-
-
+	} else if(mode == "multiple"){
+		# mutliple code
+	}
 
 }
