c6f4457359055267e926b6439547fad46b2a2d8a
Chris C <chris.c.1221@gmail.com>
2016-11-21 18:37:30 +0000

    woops"

--- inst/analysis/optimal_comorbid/sub.sh
+++ inst/analysis/optimal_comorbid/sub.sh
@@ -6,7 +6,7 @@
 #$ -V
 #$ -l mf=192G
 #$ -j y
-#$ -o /home/hpc2862/pRs/inst/analysis/optimal_comorbid/sub_log.txt
+#$ -o /home/hpc2862/repos/pRs/sub_log.txt
 #!/bin/bash
 
 cd /home/hpc2862/pRs/inst/analysis/optimal_comorbid
