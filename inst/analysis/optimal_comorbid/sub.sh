291a66e6a6d1e77d24e70ba714bad4656408d8ff
Chris C <chris.c.1221@gmail.com>
2016-11-21 18:33:31 +0000

    llots

---
+++ inst/analysis/optimal_comorbid/sub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -q abaqus.q
+#$ -l qname=abaqus.q
+#$ -cwd
+#$ -V
+#$ -l mf=192G
+#$ -j y
+#$ -o /home/hpc2862/pRs/inst/analysis/optimal_comorbid/sub_log.txt
+#!/bin/bash
+
+cd /home/hpc2862/pRs/inst/analysis/optimal_comorbid
+
+make cmb.txt
+
