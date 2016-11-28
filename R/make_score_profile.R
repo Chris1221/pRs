48a0daef5bf38b4e425eb47dafcbf9ad1bb67358
Chris C <chris.c.1221@gmail.com>
2016-11-28 12:50:12 +0000

    add in makefile and first target

---
+++ R/make_score_profile.R
@@ -0,0 +1,31 @@
+#' Take an assoc file and make an easy to use .scores file.
+#'
+#' @param assoc The file path to the assoc file.
+#'
+#' @return Nothing.
+#' @export
+
+make_score_profile <- function(assoc, file){
+
+
+	for(i in length(assoc[i])){
+
+	assertthat::assert_that(file.exists(assoc[i]))
+
+	a <- data.table::fread(assoc[i], h = T)
+
+	possible_snp_names <- c("SNP", "rsid", "snp", "legendrs")
+	snp_names <- possible_snp_names[possible_snp_names %in% colnames(a)]
+
+	possible_a2_names <- c("A2", "a2", "alternate_allele", "other_allele")
+	a2_names <- possible_a2_names[possible_a2_names %in% colnames(a)]
+
+	possible_beta_names <- c("Beta", "beta", "b", "B", "BETA", "OR", "or", "odds ratio")
+	beta_name <- possible_beta_names[possible_beta_names %in% colnames(a)]
+	
+	a %>%
+		select_(snp_names, a2_names, beta_name) %>%
+		write.table(file[i], col.names = F, row.names = F, quote = F)
+
+	}
+}
